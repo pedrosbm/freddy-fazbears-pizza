a4222904af8cb1b9e73cf8e39420eb4034faebd6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-28 11:00:46 -0300

    Projeto base do spring initializr

---
+++ src/main/java/com/psbm/fazbearentertainment/FazbearentertainmentApplication.java
@@ -0,0 +1,13 @@
+package com.psbm.fazbearentertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazbearentertainmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazbearentertainmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psbm/fazbearentertainment/FazbearentertainmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psbm.fazbearentertainment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazbearentertainmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
