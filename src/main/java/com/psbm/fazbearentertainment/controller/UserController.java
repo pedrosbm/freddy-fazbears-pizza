b97032d0f7c50a775cd7efe351ed87dcebd19074
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-03-01 11:54:06 -0300

    novas implementações

--- src/main/java/com/psbm/fazbearentertainment/controller/UserController.java
+++ src/main/java/com/psbm/fazbearentertainment/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.psbm.fazbearentertainment.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.psbm.fazbearentertainment.models.Cliente;
+
 public class UserController {
-    
+    public List<Cliente> repository = new ArrayList<>();
 }
--- src/main/java/com/psbm/fazbearentertainment/models/Cliente.java
+++ src/main/java/com/psbm/fazbearentertainment/models/Cliente.java
@@ -1,5 +1,14 @@
 package com.psbm.fazbearentertainment.models;
 
+import java.util.Random;
+
 public record Cliente(Long id_clie, String nm_clie, String email_clie, String senha_clie) {
     
+    public Cliente(Long id_clie, String nm_clie, String email_clie, String senha_clie){
+        var key = (id_clie != null) ? id_clie : Math.abs(new Random().nextLong());
+        this.id_clie = key;
+        this.nm_clie = nm_clie;
+        this.email_clie = email_clie;
+        this.senha_clie = senha_clie;
+    }
 }
