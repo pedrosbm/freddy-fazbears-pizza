536422c494254c077549006059fe5df3c476f84c
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-03-08 12:27:59 -0300

    alteração no controller e criação do repository de cliente

--- src/main/java/com/psbm/fazbearentertainment/controller/ClienteController.java
+++ src/main/java/com/psbm/fazbearentertainment/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,25 +17,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.psbm.fazbearentertainment.models.Cliente;
+import com.psbm.fazbearentertainment.repository.ClienteRepository;
 
 @RestController
 @RequestMapping(path = "/user")
 public class ClienteController {
-    // protected List<Cliente> repository = new ArrayList<>();
+    
+    @Autowired
+    private ClienteRepository repository;
 
-    // @GetMapping(path = "/{id}")
-    // @ResponseBody
-    // public ResponseEntity<Optional<Cliente>> getUser(@PathVariable long id){
-    //     try{
-    //         var user = repository.stream().filter(u -> u.id_clie().equals(id)).findFirst();
+    @GetMapping(path = "/{id}")
+    @ResponseBody
+    public ResponseEntity<Optional<Cliente>> getUser(@PathVariable long id){
+        try{
+            var user = repository.findById(id);
             
-    //         //Retorna com status 200
-    //         return ResponseEntity.ok(user);
-    //     } catch(Exception e){
-    //         e.printStackTrace();
-    //         return ResponseEntity.status(404).build();
-    //     }
-    // }
+            //Retorna com status 200
+            return ResponseEntity.ok(user);
+        } catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(404).build();
+        }
+    }
 
     // @PostMapping
     // @ResponseBody
---
+++ src/main/java/com/psbm/fazbearentertainment/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.psbm.fazbearentertainment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.psbm.fazbearentertainment.models.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
