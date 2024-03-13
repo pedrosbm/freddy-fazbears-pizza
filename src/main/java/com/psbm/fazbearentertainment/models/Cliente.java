a3411ca079fda34c613d5f4f34a20fe98b1bf8b1
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 10:35:38 -0300

    mudança na model

--- src/main/java/com/psbm/fazbearentertainment/models/Cliente.java
+++ src/main/java/com/psbm/fazbearentertainment/models/Cliente.java
@@ -3,9 +3,10 @@ package com.psbm.fazbearentertainment.models;
 import java.util.Random;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Cliente {
     
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("id_clie")
     private Long id;
 
@@ -27,12 +28,4 @@ public class Cliente {
 
     @JsonProperty("senha_clie")
     private String senha;
-
-    @JsonCreator
-    public Cliente(@JsonProperty("id_clie") Long id, @JsonProperty("nm_clie") String nome,  @JsonProperty("email_clie") String email, @JsonProperty("senha_clie") String senha) {
-        this.id = id != 0 ? id : (new Random().nextLong(0, 100000000000000l));
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
 }
