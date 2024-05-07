3332cc06307feceae6edd28ac5dd99eacbc17a6d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-07 09:15:31 -0300

    retirada de imports não utilizados

--- src/main/java/com/psbm/fazbearentertainment/repository/ClienteRepository.java
+++ src/main/java/com/psbm/fazbearentertainment/repository/ClienteRepository.java
@@ -1,12 +1,8 @@
 package com.psbm.fazbearentertainment.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.psbm.fazbearentertainment.models.Cliente;
-import com.psbm.fazbearentertainment.models.Login;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
