e9009fd8edd6cdb21903ea5faefbaeaf8d590711
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-05-07 22:37:55 -0300

    pequena alteração

--- src/main/java/com/psbm/fazbearentertainment/controller/AuthController.java
+++ src/main/java/com/psbm/fazbearentertainment/controller/AuthController.java
@@ -2,7 +2,7 @@ package com.psbm.fazbearentertainment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +18,10 @@ public class AuthController {
     @Autowired
     private ClienteRepository repository;
 
-    @GetMapping
+    @PostMapping
     private ResponseEntity<Cliente> authenticate(@RequestBody Login login) {
         try {
-            Cliente cliente = repository.findByEmailAndSenha(login.getEmail(), login.getSenha() );
+            Cliente cliente = repository.findByEmailAndSenha(login.getEmail(), login.getSenha());
 
             return ResponseEntity.ok(cliente);
         } catch (Exception e) {
